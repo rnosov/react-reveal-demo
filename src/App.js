76cb7b288acb32238c33e534288df693bfba0a0f
Roman Nosov <rnosov@gmail.com>
2017-10-20 10:27:28 +0100

    bumping demo to 0.6.0

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Fade, Flip, Rotate, Zoom } from 'react-reveal';
+import { Fade, Flip, Rotate, Zoom, Cascade } from 'react-reveal';
+import Stepper from 'react-reveal/Stepper';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function gtag() { 
@@ -17,74 +18,114 @@ if (process.env.NODE_ENV === 'production') {
   gtag('config', 'UA-107416457-1');
 }
 
-let lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ';
-    for (let i=0; i<3; i++)
-      lorem += lorem;  
-lorem = <p>{lorem}</p>;
+const lorem = <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>;
 
 class App extends Component {
 
-  state = { jumbotronLoaded: false };
-  
+  constructor(props) {
+    super(props);     
+    this.step = new Stepper()
+      .step('jumbotron', 500)       
+      .step('jumbotronText', 500)
+      .step('title', 200)
+      .step('navbar', 200)
+      .step('centralItem', 800)
+      .step('sideHeadings', 800)
+      .step('sideLists', 0)
+    ;
+  }
+
   componentDidMount() {
-    gtag('event', 'Page Load'); 
-    window.setTimeout( () => window.scrollTo( 0, 0), 100 );
+    gtag('event', 'Page Load (0.6.0)'); 
+    //window.setTimeout( () => window.scrollTo( 0, 0), 100 );
   }
 
   render() {
     const imgData = "data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==";    
     return (
       <div className="App" style={{ overflow: 'hidden' }}>
-        <Flip x tag="nav" preventReveal={!this.state.jumbotronLoaded} className="navbar navbar-expand-md navbar-dark bg-dark">
-          <div className="container">
-            <a className="navbar-brand" href="https://rnosov.github.io/react-reveal-demo/">Demo</a>            
-            <div>
-              <ul className="navbar-nav mr-auto">
-                <li className="nav-item">
-                  <a className="nav-link" href="https://www.npmjs.com/package/react-reveal">NPM Package</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="https://github.com/rnosov/react-reveal">Github Project</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="https://github.com/rnosov/react-reveal-demo">Demo Source Code</a>
-                </li>             
-              </ul>        
+        <div style={{overflow:'hidden'}}>
+          <Flip x tag="nav" step={this.step.is('navbar')} className="navbar navbar-expand-md navbar-dark bg-dark">
+            <div className="container">
+              <a className="navbar-brand" href="https://rnosov.github.io/react-reveal-demo/">Demo</a>            
+              <div>
+                <ul className="navbar-nav mr-auto">
+                  <li className="nav-item">
+                    <a className="nav-link" href="https://www.npmjs.com/package/react-reveal">NPM Package</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="https://github.com/rnosov/react-reveal">Github Repository</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="https://github.com/rnosov/react-reveal-demo">Demo Source Code</a>
+                  </li>             
+                </ul>        
+              </div>
             </div>
+          </Flip>          
+          <Zoom step={this.step.is('jumbotron')} duration={800} className="jumbotron">
+            <div className="container" style={{opacity: 1, visibility:'visible'}}>
+              <Fade top step={this.step.is('title')} tag="h1" className="text-center">React Reveal</Fade>
+                <Rotate bottom right step={this.step.is('jumbotronText')}>
+                  <p><a href="https://www.npmjs.com/package/react-reveal"> React Reveal</a> is a dead simple way to add some cool reveal on scroll animations to your React app. It's less than 2kb gzipped and specifically written for React in ES6.
+                  It was used to create vairous animations that you see on this page.
+                      Scroll down to see more. <a href="https://github.com/rnosov/react-reveal-demo">Source code of this demo</a>.            
+                    </p>
+                  <p><button onClick={ () => window.location.reload(false) } className="btn btn-outline-primary btn-lg">Refresh Page</button></p>
+                </Rotate>
+            </div>
+          </Zoom>
+          <div className="container text-center">
+            <div className="row">
+              <div className="col-lg-4">          
+                <Zoom duration={1200} delay={200}  step={this.step.is('sideHeadings')}><img alt="" className="rounded-circle" src={imgData} width="140" height="140" /></Zoom>          
+                <Flip x tag="h2" step={this.step.is('sideHeadings')}>Cross Browser</Flip>
+                <Cascade grand>
+                  <Fade left duration={1200} step={this.step.is('sideLists')}>
+                    <ul className="list-group">
+                      <li className="list-group-item">Works in any modern desktop and mobile browser</li>
+                      <li className="list-group-item">Google Chrome</li>
+                      <li className="list-group-item">Mozilla Firefox</li>
+                      <li className="list-group-item">Apple Safari</li>
+                      <li className="list-group-item">Internet Explorer 10+</li>
+                    </ul>
+                  </Fade>
+                </Cascade>
+              </div>
+              <div className="col-lg-4">                  
+                <Zoom duration={1200} delay={200} step={this.step.is('centralItem')}><img alt="" className="rounded-circle" src={imgData} width="140" height="140" /></Zoom>
+                <Flip x tag="h2" step={this.step.is('centralItem')}>Small Size</Flip>          
+                <Cascade>
+                  <Fade bottom duration={1200} step={this.step.is('centralItem')} className="list-group" tag="ul">
+                    <li className="list-group-item">React Reveal is less than 2kb gzipped</li>
+                    <li className="list-group-item">specifically written for React in ES6.</li>
+                    <li className="list-group-item">Morbi leo risus</li>
+                    <li className="list-group-item">Porta ac consectetur ac</li>
+                    <li className="list-group-item">Vestibulum at eros</li>
+                    <li className="list-group-item">felis euismod semper</li>              
+                  </Fade>
+                </Cascade>          
+              </div>
+              <div className="col-lg-4">                  
+                <Zoom duration={1200} delay={200} step={this.step.is('sideHeadings')}><img alt="" className="rounded-circle" src={imgData} width="140" height="140" /></Zoom>
+                <Flip x tag="h2" step={this.step.is('sideHeadings')}>Simple Syntax</Flip>          
+                <Cascade depth={2}>
+                  <Fade right duration={1200} step={this.step.is('sideLists')}>
+                    <ul className="list-group">
+                      <li className="list-group-item">Choose from a number of easy to remember reveal effects.</li>
+                      <li className="list-group-item">Morbi leo risus</li>
+                      <li className="list-group-item">Porta ac consectetur ac</li>
+                      <li className="list-group-item">Vestibulum at eros</li>
+                    </ul>
+                  </Fade>
+                </Cascade>          
+              </div>        
+            </div>      
           </div>
-        </Flip>
-        <Flip className="jumbotron" onReveal={ () => window.setTimeout( () => this.setState({jumbotronLoaded: true}), 800) } >
-          <div className="container">
-            <h1>React Reveal</h1>
-            <Rotate up right delay={500} wave>
-              <p><a href="https://www.npmjs.com/package/react-reveal"> React Reveal</a> is a dead simple way to add some cool reveal on scroll animations to your React app. It's less than 2kb gzipped and specifically written for React in ES6.
-              It was used to create vairous animations that you see on this page.
-                  Scroll down to see more. <a href="https://github.com/rnosov/react-reveal-demo">Source code of this demo</a>.            
-                </p>
-              <p><button onClick={ () => window.location.reload(false) } className="btn btn-primary btn-lg">Refresh Page</button></p>
-            </Rotate>
-          </div>
-        </Flip>
+        </div>
         <div className="container">
-          <Fade up wave preventReveal={!this.state.jumbotronLoaded} className="row">
-            <div className="col-lg-4">          
-              <Zoom delay={500} preventReveal={!this.state.jumbotronLoaded}><img alt="" className="rounded-circle" src={imgData} width="140" height="140" /></Zoom>
-              <h2>Cross Browser</h2>
-              <p>Works in any modern desktop and mobile browser including Internet Explorer 10. Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
-            </div>
-            <div className="col-lg-4">
-              <Zoom delay={700} preventReveal={!this.state.jumbotronLoaded}><img alt="" className="rounded-circle" src={imgData} width="140" height="140" /></Zoom>
-              <h2>Small Size</h2>
-              <p>React Reveal is less than 2kb gzipped and specifically written for React in ES6. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
-            </div>
-            <div className="col-lg-4">
-              <Zoom delay={800} preventReveal={!this.state.jumbotronLoaded}><img alt="" className="rounded-circle" src={imgData} width="140" height="140" /></Zoom>
-              <h2>Simple Syntax</h2>
-              <p>Choose from a number of easy to remember reveal effects. Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-            </div>
-          </Fade>
           <hr />
-          {Array(10).fill(void 0).map( (val, index) => 
+          {Array(...Array(10)).map( (val, index) => 
             <div key={index}>
               <Fade>{lorem}</Fade>
               <Fade down>{lorem}</Fade>
